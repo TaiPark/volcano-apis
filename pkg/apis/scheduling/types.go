6193e7f9fec4fdf9fd718f1eb9fb9751987a6795
bufan <bufan@bilibili.com>
2024-07-31 14:02:38 +0800

    Add priority in queue spec

--- pkg/apis/scheduling/types.go
+++ pkg/apis/scheduling/types.go
@@ -353,6 +353,10 @@ type QueueSpec struct {
 	// The amount of resources configured by the user. This part of resource can be shared with other queues and reclaimed back.
 	// +optional
 	Deserved v1.ResourceList `json:"deserved,omitempty" protobuf:"bytes,9,opt,name=deserved"`
+
+	// Priority define the importance of queue
+	// +optional
+	Priority int32 `json:"priority,omitempty" protobuf:"bytes,10,opt,name=priority"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
